6733da6ff762d05563f683ff212e0e3961fdd288
jared frankel <jaredfrankel10@gmail.com>
2017-09-11 19:41:48 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
-board = [" x "," x "," x ","   ","   ","   ","   ","   ","   "]
+board = 
 
-def display_board(board)
+def display_board("   ","   ","   ","   ","   ","   ","   ","   ","   ")
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
