1a027248b1fd491f3c47db1b674416c776c05614
jared frankel <jaredfrankel10@gmail.com>
2017-09-11 19:54:36 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,7 +6,49 @@ board4 =[" x "," x "," x ","   ","   ","   ","   ","   ","   "]
 board5 =["   ","   ","   ","   ","   ","   "," o "," o "," o "]
 board6 =[" x ","   ","   ","   "," x ","   ","   ","   "," x "]
 board7 =["   ","   "," o ","   "," o ","   "," o ","   ","   "]
-def display_board(board)
+def display_board(board0)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+def display_board(board1)
+    puts "   |   |   "
+    puts "-----------"
+    puts "   |   |   "
+    puts "-----------"
+    puts "   |   |   "
+def display_board(board2)
+      puts "   |   |   "
+      puts "-----------"
+      puts "   |   |   "
+      puts "-----------"
+      puts "   |   |   "
+def display_board(board3)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+def display_board(board4)
+    puts "   |   |   "
+    puts "-----------"
+    puts "   |   |   "
+    puts "-----------"
+    puts "   |   |   "
+def    display_board(board5)
+      puts "   |   |   "
+      puts "-----------"
+      puts "   |   |   "
+      puts "-----------"
+      puts "   |   |   "
+def display_board(board6)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+def display_board(board7)
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
